705897c3672a8421eafc5a9d6c1e005c43422513
bismanxdhillxn <123344647+bismanxdhillxn@users.noreply.github.com>
2025-04-15 12:35:10 +0530

    Create card.jsx

---
+++ card.jsx
@@ -0,0 +1,9 @@
+export function Card({ children }) {
+  return (
+    <div className="bg-white shadow-md rounded-xl p-4">{children}</div>
+  );
+}
+
+export function CardContent({ children, className = "" }) {
+  return <div className={`p-2 ${className}`}>{children}</div>;
+}
